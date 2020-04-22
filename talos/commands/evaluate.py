fa631244d4754d75242191b145c01fba577134c0
FlorianBury <FlorianBury@github.com>
2020-04-22 13:58:22 +0200

    Changed to F1 score for evaluation

--- talos/commands/evaluate.py
+++ talos/commands/evaluate.py
@@ -36,10 +36,10 @@ class Evaluate:
         kx, ky = kfold(x, y, folds, shuffle)
 
         for i in range(folds):
-            #y_pred = model.predict(kx[i]) >= 0.5
-            #scores = f1_score(y_pred, ky[i], average=average)
-            y_pred = asarray(model.predict(kx[i]) )
-            scores = mean_squared_error(y_pred,ky[i])
+            y_pred = model.predict(kx[i]) >= 0.5
+            scores = f1_score(y_pred, ky[i], average=average)
+            #y_pred = asarray(model.predict(kx[i]) )
+            #scores = mean_squared_error(y_pred,ky[i])
             out.append(scores)
 
             #out.append(scores * 100)
